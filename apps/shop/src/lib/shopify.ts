c46cab4e822001d18df64542877b18b03244e49b
shawn <me@bentsignal.com>
2026-02-22 08:09:17 -0500

    update shopify api version

--- apps/shop/src/lib/shopify.ts
+++ apps/shop/src/lib/shopify.ts
@@ -4,6 +4,6 @@ import { env } from "~/env";
 
 export const shopify = createStorefrontApiClient({
   storeDomain: env.SHOPIFY_STORE_DOMAIN,
-  apiVersion: "2025-10",
+  apiVersion: "2026-01",
   privateAccessToken: env.SHOPIFY_STOREFRONT_PRIVATE_TOKEN,
 });
